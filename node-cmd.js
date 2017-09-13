b5b790a7332af8cc6aad1aa351ef563b6fb15eeb
Venkataramana <nv.palthya@globaledgesoft.com>
2017-09-13 15:23:21 +0530

    first commit

---
+++ node-cmd.js
@@ -0,0 +1,14 @@
+var cmd=require('node-cmd');
+ 
+    cmd.get(
+        'pwd',
+        function(err, data, stderr){
+            console.log('the current working dir is : ',data);
+        }
+    );
+    /*cmd.get(
+        'ls',
+        function(err, data, stderr){
+            console.log('the current dir contains these files :\n\n',data);
+        }
+    );*/
